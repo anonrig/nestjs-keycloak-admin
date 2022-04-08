a01a92dff29ae4fd4428431d7403471befb0af2a
Abdoulaye Traore <traore_a@outlook.com>
2022-04-08 03:37:15 +0200

    feat: add role guard

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,12 @@
 export const KEYCLOAK_ADMIN_OPTIONS = 'KEYCLOAK_ADMIN_OPTIONS'
+
+export enum RoleMatchingMode {
+    /**
+     * Match all roles
+     */
+    ALL = 'all',
+    /**
+     * Match any roles
+     */
+    ANY = 'any',
+  }
